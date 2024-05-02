c2882609b9159550cd362f043ac76e5243af44c3
Yalalt Gandush <yalaltgandush@gmail.com>
2024-05-02 18:04:26 +0800

    comment: add comm

--- src/renderer/src/hooks/useNotesList.tsx
+++ src/renderer/src/hooks/useNotesList.tsx
@@ -2,8 +2,10 @@ import { notesAtom, selectedNoteIndexAtom } from '@/store'
 import { useAtom, useAtomValue } from 'jotai'
 
 export const useNotesList = ({ onSelect }: { onSelect?: () => void }) => {
+  // seperated component value get
   const notes = useAtomValue(notesAtom)
 
+  // iseAtom usied to same component set value and get value
   const [selectedNoteIndex, setSelectedNoteIndex] = useAtom(selectedNoteIndexAtom)
 
   const handleNoteSelect = (index: number) => async () => {
