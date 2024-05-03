3f022cce62ba745d55048927dfc2b4ae8764634d
Yalalt Gandush <yalaltgandush@gmail.com>
2024-05-03 22:51:10 +0800

    hook: add on button

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components/Button/ActionButton'
+import { createEmptyNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { LuFileSignature } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreate = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreate} {...props}>
       <LuFileSignature className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
