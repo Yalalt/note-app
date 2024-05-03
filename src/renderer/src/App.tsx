3f0f836b025d55edce4f124ce78172d03600811b
Yalalt Gandush <yalaltgandush@gmail.com>
2024-05-03 13:36:56 +0800

    update: scroll jump to top

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -8,17 +8,29 @@ import {
   RootLayout,
   SideBar
 } from '@/components'
+import { useRef } from 'react'
 
 const App = () => {
+  const contentContainerRef = useRef<HTMLDivElement>(null)
+
+  const resetScroll = () => {
+    if (contentContainerRef.current) {
+      contentContainerRef.current?.scrollTo(0, 0)
+    }
+  }
+
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
         <SideBar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 space-y-1" />
+          <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
         </SideBar>
-        <Content className="p-2 border-l bg-zinc-900/50 border-l-white/20">
+        <Content
+          ref={contentContainerRef}
+          className="p-2 border-l bg-zinc-900/50 border-l-white/20"
+        >
           <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
